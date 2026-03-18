7223b28595383488b49fb0fd6794f2d1c34bc0f2
baticlean <baticlean225@gmail.com>
2026-03-18 03:53:05 +0000

    babel

--- babel.config.js
+++ babel.config.js
@@ -4,11 +4,10 @@ module.exports = function(api) {
   
   const plugins = [];
 
-  // Nettoyage intelligent en production : on supprime les logs inutiles (info, log, debug)
-  // MAIS on conserve imperativement 'error' et 'warn' pour que Sentry puisse capturer les crashs.
-  if (process.env.NODE_ENV === 'production' || process.env.BABEL_ENV === 'production') {
-    plugins.push(['transform-remove-console', { exclude: ['error', 'warn'] }]);
-  }
+  // DÉSACTIVÉ TEMPORAIREMENT POUR DÉBLOQUER LE BUILD PRODUCTION
+  // if (process.env.NODE_ENV === 'production' || process.env.BABEL_ENV === 'production') {
+  //   plugins.push(['transform-remove-console', { exclude: ['error', 'warn'] }]);
+  // }
 
   // ATTENTION : Le plugin reanimated DOIT ABSOLUMENT etre le dernier de la liste.
   // Ne jamais rien push apres cette ligne.
