6d45f1ebb994145056c62db8acc5a3f4020eb3a2
baticlean <baticlean225@gmail.com>
2026-04-27 20:42:30 +0000

    chore: update ESLint rules and remove deprecated jsconfig setting

--- eslint.config.js
+++ eslint.config.js
@@ -6,5 +6,10 @@ module.exports = defineConfig([
   expoConfig,
   {
     ignores: ['dist/*'],
+    rules: {
+      'import/no-named-as-default-member': 'off',
+      'react/no-unescaped-entities': 'off',
+      'no-unused-vars': 'warn',
+    }
   },
 ]);
